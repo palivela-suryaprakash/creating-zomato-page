3d46e734bc68608c5a5321ed4692c866d2da0ae9
iphone 14 <20031a0437@gmail.com>
2024-08-28 21:31:11 +0530

    This is my first commit

---
+++ javascript/js/ java script-series/project.js
@@ -0,0 +1,47 @@
+var restaurantList=[
+    {
+        name: 'BBC',
+        city: "mumbai",
+        location: "mumbai",
+
+    },
+    {
+        name: 'NMC',
+        city: "mumbai",
+        location: "mumbai",
+
+    },
+    {
+        name: 'ABC',
+        city: "mumbai",
+        location: "mumbai",
+
+    }
+    
+]
+console.log(restaurantList);
+//restaurantList.find()
+//restaurantList.splice(0,1);
+var array = ["deepak","suresh",{name:"deepak"},10,50,4]
+var isIncluded = array.includes(4);
+console.log(isIncluded);
+    
+
+var radius = 30;
+console.log(Math.PI);
+var value = Math.random();
+console.log(value);
+//to generate round numbers
+//console.log(Math.floor(5.50));
+//To create a string method 
+var text = "I love my darling swetha"
+var output = text.toLowerCase();
+console.log(output);
+var output = text.toUpperCase();
+console.log(output);
+var output = text.split(" ");
+console.log(output);
+var output = text.split(".");
+output = output.join("-");
+output = output.toLowerCase();
+console.log(output);
---
+++ javascript/js/index.js
@@ -0,0 +1,13 @@
+var restaurantlist=[
+    {
+        name: "ABC",
+        city: "mumbai",
+        location:"central mumbai",
+        isVeg: true,
+        
+    },
+    { name: "Green Cafe", city: "Pune", Locality:"andhra pradesh",isVeg: false },
+    { name: "Royal Cafe", city:"narasaraopet",Locality: "swinchan", isVeg: true},
+];
+
+console.log(restaurantlist);
